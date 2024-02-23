56df65007ebc20f563754e5b0e647af40494a73f
elimunga <149093431+elimunga@users.noreply.github.com>
2024-02-23 15:35:58 +0300

    Data types assignment
    Assignment 1 on data types on dart

---
+++ datatypes.dart
@@ -0,0 +1,25 @@
+void main(){
+  //Declaring and initializing an integer variable
+  int age=19;
+  //Declaring and initializing a double variable
+  double weight=50.5;
+  //Declaring and initializing a string variable
+  String name="Eli Munga";
+  //Declaring and initializing a boolean variable
+  bool ismarried=false;
+  //Declaring and initializing a List variable
+  List<String> hobbies=["Football","swimming","Hockey"];
+  //Declaring and initializing a Map variable
+  Map<String, int> scores={"Mathematics":88,"Science":85,"Language":80,};
+
+  
+  //Output
+
+  print("Name is $name"); //string
+  print("Age is $age"); //int
+  print("Weight is $weight"); //double
+  print("Marriage status is $ismarried"); //bool
+  print("Hobbies are: $hobbies"); //List
+  print("Scores are: $scores"); //Map
+
+}
